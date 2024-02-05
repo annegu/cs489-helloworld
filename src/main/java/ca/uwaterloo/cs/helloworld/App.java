b403c48a37cb394f12ba99f2fda3fee0563071be
Anne Zyllee <anneg@v1040-wn-rt-a-109-178.campus-dynamic.uwaterloo.ca>
2024-02-05 14:05:50 -0500

    breaking change

--- src/main/java/ca/uwaterloo/cs/helloworld/App.java
+++ src/main/java/ca/uwaterloo/cs/helloworld/App.java
@@ -8,5 +8,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello, world!" );
+        int gonna_break = 100 / 0;
+
+        System.out.println( gonna_break );
     }
 }
